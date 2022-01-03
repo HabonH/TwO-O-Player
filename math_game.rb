f40cc885c6ea1895559411934ed3404013046de1
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2022-01-03 16:12:41 -0500

    Develop Game and Player class

--- math_game.rb
+++ math_game.rb
@@ -16,31 +16,77 @@
 # winner - would be executed once a player has reached 0 lives and will show winner's score
 
 class Game
-attr_accessor :player
+  attr_reader :player1, :player2
+  attr_accessor :current_player
 
-def initialize(player, current_score)
-  @player = player
-  @current_score = 3
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @current_player = player1
+  end
+  def to_s
+    "I am #{self.player1}!" + " " + "I am #{self.player2}!"
+  end
+  def start
+    while player1.lives > 0 && player2.lives > 0 
+      # continue the game
+    end
+  end
 end
+
+class Player
+  # name, lives
+  attr_reader :name, :lives 
+  def initialize(name)
+    @name = name
+    @lives = 3
+  end
+  
+  def decrease_life
+    @lives = @lives - 1
+  end
 end
 
 class Question
-def question
-  # deals with current player by then providing the question
-end
+  def initialize(num1, num2, answer)
+    @num1 = num1
+    @num2 = num2
+    @answer = answer
+  end
+  
+  def question
+    "What is #{self.num1} + #{self.num2}?"
+    response = gets.chomp
+    if response == answer
+      "You're right!"
+    else
+      "You're wrong, lose a life"
+    end
+  end
 end
 
 class Score
-def score
-  # deals with user input and output
-end
+  def win_round?
+    if @current_score < 3 
+      @current_score += 1
+    else @current_score
+    end
+  end
+  def lose_round?
+    if @current_score == 0
+      puts "Winner announcement"
+    else @current_score -= 1
+    end
+  end
 end
 
 class Winner
-def winner
-end
+  def winner
+  end
 end
 
-# player is relevant to question, score and winner
-# question is relevant to score
-# score is relevant to winner
+player1, player2 = Game.new("player1", "player2")
+puts player1
+puts player2
+question1 = Question.new(2, 4, 6)
+p question1
